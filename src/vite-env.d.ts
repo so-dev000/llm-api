d00e9b19f2090af9f86ffc9155b461db58f3fd82
Nakamura Sota <sota_nakamura@demia.co.jp>
2024-06-09 13:21:12 +0900

    remove type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_SUPABASE_PROJECT_URL: string;
-  readonly VITE_SUPABASE_API_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
